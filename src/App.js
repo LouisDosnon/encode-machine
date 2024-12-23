8237b6bfee5c0c70af61146fa89bf7f48260d566
LouisDosnon <louisdosnon@gmail.com>
2024-12-23 17:33:46 +0100

    add formulaire

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Encode from "./Component/Encode";
 
--- src/Component/Encode.js
+++ src/Component/Encode.js
@@ -6,7 +6,12 @@ class Encode extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            key1: { letter: "A", value: 1 }
+            key1: { letter: "A", value: 1 },
+            key2: { letter: "A", value: 1 },
+            key3: { letter: "A", value: 1 },
+            key4: { letter: "A", value: 1 },
+            text: "",
+            encodeText: ""
         }
         this.changeKey1 = this.changeKey1.bind(this)
     }
@@ -20,6 +25,22 @@ class Encode extends React.Component {
                         return (<option key={"1"+letter.letter} value={letter}>{letter.letter}</option>)
                     })}
                 </select>
+                <select id="key2" onChange={this.changeKey2}>
+                    {alphabet.map((letter) => {
+                        return (<option key={"2"+letter.letter} value={letter}>{letter.letter}</option>)
+                    })}
+                </select>
+                <select id="key3" onChange={this.changeKey3}>
+                    {alphabet.map((letter) => {
+                        return (<option key={"3"+letter.letter} value={letter}>{letter.letter}</option>)
+                    })}
+                </select><select id="key4" onChange={this.changeKey4}>
+                    {alphabet.map((letter) => {
+                        return (<option key={"4"+letter.letter} value={letter}>{letter.letter}</option>)
+                    })}
+                </select>
+                <input type="text" onChange={this.changeText}/>
+                <button onClick={this.encode}>encode</button>
             </div>
         )
     }
@@ -30,6 +51,43 @@ class Encode extends React.Component {
         }, () => {
             console.log(this.state);
         })
+    }
+
+    changeKey2(e) {
+        this.setState({
+            key2: e.target.value
+        }, () => {
+            console.log(this.state);
+        })
+    }
+
+    changeKey3(e) {
+        this.setState({
+            key3: e.target.value
+        }, () => {
+            console.log(this.state);
+        })
+    }
+
+    changeKey4(e) {
+        this.setState({
+            key4: e.target.value
+        }, () => {
+            console.log(this.state);
+        })
+    }
+
+    changeText(e) {
+        this.setState({
+            text: e.target.value
+        }, () => {
+            console.log(this.state);
+        })
+    }
+
+    encode() {
+        console.log((this.state.key1.value * this.state.key2.value)
+            /(this.state.key3.value * this.state.key4.value))
     }
 }
 
